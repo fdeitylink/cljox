72eaef13bfc1cb6bdb8f1bee8909dcfee087cd43
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-02-18 01:11:30 -0500

    update core.clj to execute Lox from files or REPL
    currently prints errors, if any, otherwise tokens

--- src/cljox/core.clj
+++ src/cljox/core.clj
@@ -1,4 +1,36 @@
-(ns cljox.core)
+(ns cljox.core
+  (:gen-class)
+  (:require [cljox.error :as error]
+            [cljox.scanner :as scanner]))
+
+(defn- print-errors
+  [errors]
+  (doseq [error errors] (println (error/pretty-str error))))
+
+(defn run
+  "Executes `input` as a Lox program"
+  [input]
+  (let [{::scanner/keys [tokens errors]} (scanner/scan input)]
+    (if (seq errors)
+      (print-errors errors)
+      (doseq [token tokens] (println token)))))
+
+(defn run-file
+  "Executes `file` as a Lox program"
+  [file]
+  (run (slurp file)))
+
+(defn run-prompt
+  "Runs an interactive prompt to execute Lox lines until EOF"
+  []
+  (println "> ")
+  (flush)
+  (when-let [line (read-line)]
+    (run line)
+    (recur)))
 
 (defn -main
-  [& args])
+  "Executes the Lox interpreter"
+  ([] (run-prompt))
+  ([file] (run-file file))
+  ([_ _ & _] (println "Usage: cljox [script]")))
