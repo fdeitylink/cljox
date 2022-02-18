cd1c85d185c9029bb52d216c09bbfeb59ae99510
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-02-18 01:34:27 -0500

    add ast.clj for representing and printing Lox AST

---
+++ src/cljox/ast.clj
@@ -0,0 +1,55 @@
+(ns cljox.ast
+  (:require [cljox.token :as token]
+            clojure.string))
+
+(defn literal
+  "Creates a literal expression of `value`"
+  [value]
+  #::{:type ::literal
+      :value value})
+
+(defn grouping
+  "Creates a grouping expression of `expression`"
+  [expression]
+  #::{:type ::grouping
+      :expression expression})
+
+(defn unary
+  "Creates a unary expression of `operator` `right`"
+  [operator right]
+  #::{:type ::unary
+      :operator operator
+      :right right})
+
+(defn binary
+  "Creates a binary expression of `left` `operator` `right`"
+  [left operator right]
+  #::{:type ::binary
+      :left left
+      :operator operator
+      :right right})
+
+(defmulti pretty-str
+  "Converts an ast into a parenthesized Polish form string"
+  ::type)
+
+(defn parenthesize
+  "Parenthesizes `name` and `expressions` as a string"
+  [name & expressions]
+  (str "(" name " " (clojure.string/join " " (map pretty-str expressions)) ")"))
+
+(defmethod pretty-str ::literal
+  [{::keys [expression]}]
+  (pr-str expression))
+
+(defmethod pretty-str ::grouping
+  [{::keys [expression]}]
+  (parenthesize "group" expression))
+
+(defmethod pretty-str ::unary
+  [{::keys [operator right]}]
+  (parenthesize (::token/lexeme operator) right))
+
+(defmethod pretty-str ::binary
+  [{::keys [left operator right]}]
+  (parenthesize (::token/lexeme operator) left right))
