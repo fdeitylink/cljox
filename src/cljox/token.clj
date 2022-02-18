3681c855561093570796b8209dc698acc52d1913
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-02-18 01:07:04 -0500

    add token.clj for representing Lox code tokens

---
+++ src/cljox/token.clj
@@ -0,0 +1,47 @@
+(ns cljox.token)
+
+(def types
+  "Set of valid token types"
+  #{::left-paren ::right-paren
+    ::left-brace ::right-brace
+    ::comma ::dot
+    ::minus ::plus
+    ::semicolon
+    ::slash ::star
+
+    ::bang ::bang-eq
+    ::eq ::eq-eq
+    ::gt ::gte
+    ::lt ::lte
+
+    ::identifier
+    ::string
+    ::number
+
+    ::and
+    ::class
+    ::else
+    ::false
+    ::fun
+    ::for
+    ::if
+    ::nil
+    ::or
+    ::print
+    ::return
+    ::super
+    ::this
+    ::true
+    ::var
+    ::while
+
+    ::eof})
+
+(defn token
+  "Creates a token with the given properties"
+  [type lexeme literal start end]
+  #::{:type type
+      :lexeme lexeme
+      :literal literal
+      :start start
+      :end end})
