758843042e44a9344cc8d7c042e055d1c8be54e2
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-02-18 01:07:51 -0500

    add error.clj for representing Lox code errors

---
+++ src/cljox/error.clj
@@ -0,0 +1,17 @@
+(ns cljox.error
+  (:require [cljox.location :as location]))
+
+(defn error
+  "Creates a new error with the given `type` and `start` and `end`
+  locations and any extra properties in `keyvals`."
+  [type start end & keyvals]
+  (apply assoc #::{:type type :start start :end end} keyvals))
+
+(defmulti fmt
+  "Returns an error message according to the error's type"
+  ::type)
+
+(defn pretty-str
+  "Formats `error` as a string with location and message"
+  [{::keys [start end] :as error}]
+  (str (location/pretty-str start end) ": " (fmt error)))
