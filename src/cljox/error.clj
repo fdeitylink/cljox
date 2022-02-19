d56ffafd71a3383fc7532f4dec4d8bb1b87d3aa9
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-02-19 17:57:23 -0500

    add error/runtime-error for runtime exceptions

--- src/cljox/error.clj
+++ src/cljox/error.clj
@@ -1,11 +1,6 @@
 (ns cljox.error
-  (:require [cljox.location :as location]))
-
-(defn error
-  "Creates a new error with the given `type` and `start` and `end`
-  locations and any extra properties in `keyvals`."
-  [type start end & keyvals]
-  (apply array-map ::type type ::start start ::end end keyvals))
+  (:require [cljox.location :as location]
+            [cljox.token :as token]))
 
 (defmulti fmt
   "Returns an error message according to the error's type"
@@ -15,3 +10,16 @@
   "Formats `error` as a string with location and message"
   [{::keys [start end] :as error}]
   (str (location/pretty-str start end) ": " (fmt error)))
+
+(defn error
+  "Creates a new error with the given `type` and `start` and `end`
+  locations and any extra properties in `keyvals`."
+  [type start end & keyvals]
+  (apply array-map ::type type ::start start ::end end keyvals))
+
+(defn runtime-error
+  "Creates an error of type `type` located at `token` with any extra
+  properties in `keyvals` and throws it inside an [[ExceptionInfo]]"
+  [type token & keyvals]
+  (let [error (apply error type (::token/start token) (::token/end token) keyvals)]
+    (throw (ex-info (fmt error) error))))
