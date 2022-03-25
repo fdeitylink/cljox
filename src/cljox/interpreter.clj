142708cbf94c54701f94a90ca0bde0d85a44cdb4
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2022-03-25 16:33:35 -0400

    update interpreter.clj to evaluate statements

--- src/cljox/interpreter.clj
+++ src/cljox/interpreter.clj
@@ -1,7 +1,9 @@
 (ns cljox.interpreter
   (:require [cljox.ast :as ast]
+            [cljox.environment :as environment]
             [cljox.error :as error]
-            [cljox.token :as token]))
+            [cljox.token :as token]
+            [clojure.string :as s]))
 
 (defmethod error/fmt ::nonnumeric-unary-operand
   [{::keys [operator right] :as error}]
@@ -18,6 +20,17 @@
 (defmethod error/fmt ::division-by-zero
   [{::keys [left] :as error}]
   (format "division of '%s' by 0" (pr-str left)))
+
+(defn state
+  "Creates a new interpreter state"
+  []
+  #::{:result nil
+      :environment (environment/environment)
+      :error nil})
+
+(defn clear-error
+  [state]
+  (assoc state ::error nil))
 
 (defn- numeric-operator
   ([operator f r]
@@ -60,50 +73,102 @@
                         (/ l r)))
                     l r))
 
-(defmulti ^:private evaluate ::ast/type)
+(defmulti ^:private evaluate
+  (fn [state ast] (::ast/type ast)))
 
 (defmethod evaluate ::ast/literal
-  [{::ast/keys [value]}]
-  value)
+  [state {::ast/keys [value]}]
+  (assoc state ::result value))
+
+(defmethod evaluate ::ast/var
+  [{::keys [environment] :as state} {::ast/keys [name]}]
+  (assoc state ::result (environment/get-var environment name)))
 
 (defmethod evaluate ::ast/grouping
-  [{::ast/keys [expression]}]
-  (evaluate expression))
+  [state {::ast/keys [expression]}]
+  (evaluate state expression))
 
 (defmethod evaluate ::ast/unary
-  [{::ast/keys [operator right]}]
-  (let [r (evaluate right)]
-    (case (::token/type operator)
-      ::token/bang (not r)
-      ::token/minus (numeric-operator operator - r))))
+  [state {::ast/keys [operator right]}]
+  (let [state (evaluate state right)
+        r (::result state)]
+    (assoc state ::result
+           (case (::token/type operator)
+             ::token/bang (not r)
+             ::token/minus (numeric-operator operator - r)))))
 
 (defmethod evaluate ::ast/binary
-  [{::ast/keys [left operator right]}]
-  (let [l (evaluate left)
-        r (evaluate right)]
-    (case (::token/type operator)
-      ::token/minus (numeric-operator operator - l r)
-      ::token/plus (add operator l r)
-      ::token/slash (divide operator l r)
-      ::token/star (numeric-operator operator * l r)
-      ::token/bang-eq (not= l r)
-      ::token/eq-eq (= l r)
-      ::token/gt (numeric-operator operator > l r)
-      ::token/gte (numeric-operator operator >= l r)
-      ::token/lt (numeric-operator operator < l r)
-      ::token/lte (numeric-operator operator <= l r)
-      ::token/comma r)))
+  [state {::ast/keys [left operator right]}]
+  (let [state (evaluate state left)
+        l (::result state)
+        state (evaluate state right)
+        r (::result state)]
+    (assoc state ::result
+           (case (::token/type operator)
+             ::token/minus (numeric-operator operator - l r)
+             ::token/plus (add operator l r)
+             ::token/slash (divide operator l r)
+             ::token/star (numeric-operator operator * l r)
+             ::token/bang-eq (not= l r)
+             ::token/eq-eq (= l r)
+             ::token/gt (numeric-operator operator > l r)
+             ::token/gte (numeric-operator operator >= l r)
+             ::token/lt (numeric-operator operator < l r)
+             ::token/lte (numeric-operator operator <= l r)
+             ::token/comma r))))
 
 (defmethod evaluate ::ast/ternary
-  [{::ast/keys [test then else]}]
-  (if (evaluate test)
-    (evaluate then)
-    (evaluate else)))
+  [state {::ast/keys [test then else]}]
+  (let [state (evaluate state test)]
+    (evaluate state (if (::result state) then else))))
+
+(defmethod evaluate ::ast/assignment
+  [state {::ast/keys [name value]}]
+  (let [state (evaluate state value)
+        env (::environment state)
+        val (::result state)]
+    (assoc state
+           ::environment (environment/assign-var env name val)
+           ::result val)))
+
+(defmethod evaluate ::ast/expression-statement
+  [state {::ast/keys [expression]}]
+  (-> state
+      (evaluate expression)
+      (assoc ::result nil)))
+
+(defmethod evaluate ::ast/print-statement
+  [state {::ast/keys [expression]}]
+  (let [state (evaluate state expression)]
+    (-> state ::result stringify println)
+    (assoc state ::result nil)))
+
+(defmethod evaluate ::ast/block
+  [state {::ast/keys [statements]}]
+  (loop [state (update state ::environment environment/push-scope)
+         statements statements]
+    (if-let [statement (first statements)]
+      (recur (evaluate state statement) (rest statements))
+      (update state ::environment environment/pop-scope))))
+
+(defmethod evaluate ::ast/var-statement
+  [state {::ast/keys [name initializer]}]
+  (let [state (if initializer (evaluate state initializer) state)
+        env (::environment state)]
+    (assoc state
+           ::environment (if initializer
+                           (environment/define-var env name (::result state))
+                           (environment/declare-var env name))
+           ::result nil)))
 
 (defn interpret
-  "Interprets `ast` into a map of ::result and ::error"
-  [ast]
-  (try
-    {::result (evaluate ast) ::error nil}
-    (catch clojure.lang.ExceptionInfo e
-      {::result nil ::error (ex-data e)})))
+  "Interprets `statements` and returns an error if any, otherwise nil"
+  [state statements]
+  (reduce
+   (fn [state statement]
+     (try
+       (evaluate state statement)
+       (catch clojure.lang.ExceptionInfo e
+         (reduced (assoc state ::error (ex-data e) ::result nil)))))
+   state
+   statements))
